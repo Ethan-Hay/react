1849e991a732ea3776a59583b5b432e8a6ff0aea
Ethan Hay <ethanhay42@gmail.com>
2024-01-29 19:29:14 -0600

    update

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,26 +1,34 @@
-import React,{Component} from "react";
+import React, { Component } from 'react';
+import Medal from './Medal';
 
-class Country extends Component{
-    state = {
-        name: 'United States',
-        gold: 0
-    }
-    
-    handleIncrement = () =>{
-        console.log(this.state.gold);
-        this.setState({gold: this.state.gold +1});
-    }
-    render(){
-        return(<div>
-            <div>
-                {this.state.name}
-            </div>
-            <div>
-                {this.state.gold}
-            </div>
-            <button onClick={this.handleIncrement}>+</button>
-        </div>)
-    }
+class Country extends Component {
+  getMedalsTotal(country, medals) {
+    let sum = 0;
+    medals.forEach(medal => { sum += country[medal.name]; });
+    return sum;
+  }
+  render() { 
+    const { country, medals, onIncrement, onDecrement } = this.props;
+    return (
+      <div className="country">
+        <div className="name">
+          { country.name }
+          <span className="badge">
+            { this.getMedalsTotal(country, medals) }
+          </span>
+        </div>
+        { medals.map(medal =>
+          <Medal 
+            key={ medal.id } 
+            country={ country } 
+            medal={ medal } 
+            onIncrement={ onIncrement } 
+            onDecrement={ onDecrement } />
+        ) }
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default Country;
